64c129064e4f7b5ce3c83f811a7d40e40ee8ada8
f1309203474 <1309203474@qq.com>
2019-02-27 09:11:38 +0800

    Merge branch 'master' of https://github.com/quwenGoGoGo/quwenDev into fyf
    # Conflicts:
    #	.idea/misc.xml

--- quwen-db/src/main/java/quwen/db/repository/CollectRepository.java
+++ quwen-db/src/main/java/quwen/db/repository/CollectRepository.java
@@ -1,10 +1,9 @@
 package quwen.db.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
 import quwen.db.domain.Collect;
-import quwen.db.domain.User;
-import quwen.db.domain.News;
+
 
 import java.util.List;
 
