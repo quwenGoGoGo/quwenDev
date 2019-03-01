61e199d425647efdd0dc9ea49a29a9cc1990908f
Zhjiao <1156759563@qq.com>
2019-03-01 10:16:39 +0800

    checkout

--- quwen-web/src/main/resources/static/js/api/api.js
+++ quwen-web/src/main/resources/static/js/api/api.js
@@ -3,6 +3,7 @@ var AdminApiRoot = "http://localhost:8081";
 var login = AdminApiRoot+"/authlogin";
 var logout= AdminApiRoot+"/logout";
 
+
 var home = AdminApiRoot+"/news/index";
 export  {
     login,
